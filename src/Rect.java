5cab6693f74da38a2ff9f5efe1c2288e6cdda04c
Han shenquan <1173956412@qq.com>
2016-06-13 21:11:27 +0800

    1
    1

---
+++ src/Rect.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Administrator on 2016/5/12.
+ */
+public class Rect extends Shape {
+//    public int lon;
+//    private int lon;
+    private  final int mm =3;
+    public static void main(String[] args) {
+        Rect rect = new Rect();
+        rect.f();
+        rect.setLon();
+        System.out.println(rect.getLon());
+    }
+
+    /*@Override
+    public void setLon() {
+        lon = 2;
+    }
+    public int getLon(){
+        return lon;
+    }*/
+    private void f(){
+
+    }
+}
---
+++ src/Shape.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Administrator on 2016/5/12.
+ */
+public class Shape {
+    private int lon;
+    private int lat;
+
+    public void setLon() {
+        lon = 1;
+    }
+
+    public int getLon() {
+        return lon;
+    }
+
+
+}
